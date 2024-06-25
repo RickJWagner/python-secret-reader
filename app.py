cd252ac2b4830717b58f9918992cde5d5b5ad1a5
RickJWagner <rwagner@redhat.com>
2024-06-25 13:18:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+import os
+import subprocess
+import json
+from hydra import HydraApi
+from hydra.auth import RedHatSSOAuth
+from hydra.sso import get_token, throw_if_no_token
+
 print ("starting!")
-print ("in the middle!")
+
+
+hydra_user = os.environ["HYDRA_USERNAME"]
+hydra_pwd = os.environ["HYDRA_PASSWORD"]
+
+
+print (hydra_user)
 print ("ending")
